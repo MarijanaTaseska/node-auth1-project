d7667d1136a7c6428fee6921fd3670762472f4b2
Marijana Taseska <marijana.taseska@gmail.com>
2024-05-19 09:44:47 -0700

    login completed

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -6,7 +6,7 @@ const bcrypt = require('bcryptjs')
 const {
   checkPasswordLength,
   checkUsernameFree,
-  checkUsernameExists} = require('./auth-middleware')
+  checkUsernameExists } = require('./auth-middleware')
 
 
 /**
@@ -31,14 +31,14 @@ const {
     "message": "Password must be longer than 3 chars"
   }
  */
-router.post('/register',checkPasswordLength,checkUsernameFree, (req, res, next) => {
-const {username, password} = req.body
-const hash = bcrypt.hashSync(password, 8)
+router.post('/register', checkPasswordLength, checkUsernameFree, (req, res, next) => {
+  const { username, password } = req.body
+  const hash = bcrypt.hashSync(password, 8)
 
-User.add({username, password:hash})
-.then(saved => {
-  res.status(201).json(saved)
-}).catch(next)
+  User.add({ username, password: hash })
+    .then(saved => {
+      res.status(201).json(saved)
+    }).catch(next)
 })
 
 /**
@@ -56,8 +56,14 @@ User.add({username, password:hash})
     "message": "Invalid credentials"
   }
  */
-router.post('/login',checkUsernameExists, (req, res, next) => {
-  res.json('login')
+router.post('/login', checkUsernameExists, (req, res, next) => {
+  const { password } = req.body
+if( bcrypt.compareSync(password, req.user.password)){
+ req.session.user = req.user
+ res.json({ message: `Welcome ${req.user.username}`})
+}else{
+  next({status:401, message:'Invalid credentials'})
+}
 })
 
 /**
