b6944dd8f791ae8659a87f5383744a287989d6a1
Marijana Taseska <marijana.taseska@gmail.com>
2024-05-19 12:23:32 -0700

    Module1 Project

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -9,8 +9,11 @@ const User = require('../users/users-model')
   }
 */
 function restricted(req, res, next) {
-  console.log('restrictel middleware')
-  next()
+  if (req.session.user) {
+    next()
+  } else {
+    next({ status: 401, message: 'You shall not pass!' })
+  }
 }
 
 /*
@@ -63,8 +66,8 @@ async function checkUsernameExists(req, res, next) {
 */
 function checkPasswordLength(req, res, next) {
   if (!req.body.password || req.body.password.length < 3) {
-    next({ message: "Password must be longer than 3 chars", status:422 })
-  }else{
+    next({ message: "Password must be longer than 3 chars", status: 422 })
+  } else {
     next()
   }
 }
--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -82,7 +82,18 @@ if( bcrypt.compareSync(password, req.user.password)){
   }
  */
 router.get('/logout', (req, res, next) => {
-  res.json('logout')
+  if(req.session.user){
+    req.session.destroy(err => {
+      if(err){
+        next(err)
+      }else{
+        res.json({ status:200, message:"logged out"})
+      }
+    })
+  }else{
+    res.json({status:200, message:'no session'})
+  }
+
 })
 
 
